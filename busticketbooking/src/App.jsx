7e7c18fe104f64461a5d3b4d442c1b9642f89ab9
Arunkumar <pikkalaarunkumar@gmail.com>
2025-03-24 17:15:16 +0530

    main commit

---
+++ busticketbooking/src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Routes , Route} from "react-router-dom"
+
+function App() {
+
+  return (
+    <>
+    <main className="w-full flex flex-col bg-neutral-50 min-h-screen">
+      this is main section
+      {/* Navbar */}
+      This is navbar
+
+      {/* Routes */}
+      <Routes>
+
+      
+      </Routes>
+
+      {/* Footer */}
+      </main>
+    </>
+  )
+}
+
+export default App
