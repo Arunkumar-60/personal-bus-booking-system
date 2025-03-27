10d458cbd33f278cfa43525b8d4ac6f474161da6
Arunkumar <pikkalaarunkumar@gmail.com>
2025-03-27 14:59:31 +0530

    Update Hero.jsx

--- busticketbooking/src/pages/home/Hero.jsx
+++ busticketbooking/src/pages/home/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {easeInOut, motion} from 'framer-motion';
 import RootLayout from '../../layout/RootLayout';
+import Search from './search/Search';
 
 const Hero = () => {
 
@@ -11,20 +12,41 @@ const Hero = () => {
   return (
     <motion.div
         className='w-full flex-1 h-screen 
-        bg-[url("./assets/heroBg.png")] 
+        bg-[url("./assets/pexels-eslames1-28310142.jpg")] 
         bg-cover bg-no-repeat bg-top relative'
 
         initial="hidden"
         animate="visible"
+        exit="hidden"
         variants={variants}
         transition={{duration:0.85 , ease:easeInOut}}
     >
         <RootLayout className="absolute top-0 left-0 w-full h-full py-[9ch] 
-        bg-gradient-to-b from-neutral-50/70 via-neutral-50/15 to-neutral-50/5
+        bg-gradient-to-b from-neutral-50/30 via-neutral-50/10 to-transparent
         flex items-center justify-start text-center flex-col gap-9">
             {/* {title section} */}
-            
+            <div className="space-y-2">
+            <motion.p
+                initial={{opacity:0,y:-800}}
+                animate={{opacity:1,y:0}}
+                exit={{opacity:0,y:-800}}
+                transition={{duration:2, ease:"easeOut"}}
+                className='text-neutral-50 text-lg font-medium capitalize'>
+                    get your bus ticket
+                </motion.p>
+                
+                <motion.h1
+                initial={{opacity:0,y:-800}}
+                animate={{opacity:1,y:0}}
+                exit={{opacity:0,y:-800}}
+                transition={{duration:2, ease:"easeOut"}}
+                className='text-neutral-800 text-5xl font-bold capitalize'>
+                    find best bus for you
+                </motion.h1>
+                
+            </div>
             {/* {search section} */}
+            <Search/>
 
         </RootLayout>
     </motion.div>
